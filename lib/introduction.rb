4b0c5c10109195b4dfcc7e9916e6b50c0b753bf8
placid-prototype-6143 <sdsj17@yahoo.com>
2020-04-21 16:50:20 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction
+    puts "Hi my name is #{name}."
+end
+introduction("kiara")
+
+
+def introduction_with_language(name, language)
+    puts "Hi My name is #{name} and I am learning to program #{language}."
+end 
+introduction_with_language("Steven", Ruby)
