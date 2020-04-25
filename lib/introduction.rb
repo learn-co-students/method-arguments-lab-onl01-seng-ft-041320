e78dfff63bd4272b951176e57149ccbc54fa9199
Steven Springs Jr <sdsj17@yahoo.com>
2020-04-25 05:29:20 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,11 @@
-def introduction
-    puts "Hi my name is #{name}."
+def introduction(name)
+   puts "Hi, my name is #{name}."
 end
-introduction("kiara")
 
+introduction("Josh")
 
-def introduction_with_language(name, language)
-    puts "Hi My name is #{name} and I am learning to program #{language}."
-end 
-introduction_with_language("Steven", Ruby)
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan", "Ember.js")
